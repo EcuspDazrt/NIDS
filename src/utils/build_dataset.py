3314deb7c6a80dd8e97c7136bf914ff14f0296fa
EcuspDazrt <lmcau619@evit.edu>
2026-02-12 13:53:18 -0700

    separate utility functions

---
+++ src/utils/build_dataset.py
@@ -0,0 +1,23 @@
+from torch.utils.data import TensorDataset, DataLoader
+from sklearn.preprocessing import StandardScaler
+from torch import tensor, float32
+import joblib as jb
+
+def create_dataset(features, batch_size=0, loader=True, training=False):
+    if training:
+        scaler = StandardScaler()
+        X_np = scaler.fit_transform(features)
+        jb.dump(scaler, 'ae_scaler.pkl')
+    else:
+        scaler = jb.load("ae_scaler.pkl")
+        EXPECTED_COLUMNS = list(scaler.feature_names_in_)
+        features = features[EXPECTED_COLUMNS]
+        X_np = scaler.transform(features)
+
+    X = tensor(X_np, dtype=float32)
+
+    if loader:
+        dataset = TensorDataset(X)
+        loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+        return loader
+    return X
